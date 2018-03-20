121cb8868cc6bd480ec57abf3f865537952f2171
DamianAllende <amigosaburridos@gmail.com>
2018-03-20 15:30:37 -0600

    styles Post

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,9 +4,9 @@ class Post extends Component{
 	render(){
 		return (
 			<div className="post">
-				<header>
+				<header className='post__header'>
 					<div>
-						<img src='https://scontent.fmex11-1.fna.fbcdn.net/v/t1.0-1/p100x100/26904508_10156429668511323_5866309676974531495_n.png?oh=0860b50dc03a86826bc8c2b0d4af61c7&oe=5B28EA9A' />
+						<img className='post__image' src='https://scontent.fmex11-1.fna.fbcdn.net/v/t1.0-1/p100x100/26904508_10156429668511323_5866309676974531495_n.png?oh=0860b50dc03a86826bc8c2b0d4af61c7&oe=5B28EA9A' />
 					</div>
 					<div>
 						<h3 className="post__title">The Guardian</h3>
@@ -16,6 +16,13 @@ class Post extends Component{
 				<div className="post__content">
 					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make</p>
 				</div>
+				<footer>
+					<ul className="post__actions">
+						<li className=""><a href=""><i className='fa fa-thumbs-up'></i>Me Gusta</a></li>
+						<li className=""><a href=""><i className='fa fa-comment-o'></i>Comentarios</a></li>
+						<li className=""><a href=""><i className='fa fa-share'></i>Compartir</a></li>
+					</ul>
+				</footer>
 			</div>
 		);
 	}	
