0cbcaa2df87321eb4a58f404d7de2a9f642c9942
DamianAllende <amigosaburridos@gmail.com>
2018-03-21 10:01:48 -0600

    Resturctura HTML de Header.js y estilos css

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,20 @@ import React, { Component } from 'react';
 class Header extends Component{
 	render(){
 		return(
-			<nav className='grid grid--judtify'>
-				<div>
-					<img className='header__image' src="https://www.inria.fr/extension/site_inria/design/site_inria/images/social_networks/facebook_square.png" / >
-					<form>
-						<div className='form'>
-							<input type='text' placeholder='Search'/>
-							<button>
-								<i className='fa fa-search'></i>
-							</button>
-						</div>
-					</form>
-				</div>
-				<div></div>
-			</nav>
+			<nav className='grid grid--justify header'>
+		        <div className='grid'>
+		          <img className='header__image' src='https://upload.wikimedia.org/wikipedia/commons/c/c2/F_icon.svg' />
+		          <form>
+		            <div className='form grid'>
+		              <input type='text' placeholder='Search' />
+		              <button>
+		                <i className='fa fa-search'></i>
+		              </button>
+		            </div>
+		          </form>
+		        </div>
+		        <div></div>
+		      </nav>
 		);
 	}
 }
