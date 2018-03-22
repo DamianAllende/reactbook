8ca5ae38bec1a24f184f246cbe61ad960f92b903
DamianAllende <amigosaburridos@gmail.com>
2018-03-22 12:01:51 -0600

    Configurar index.js Router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import { HashRouter } from 'react-router-dom';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class AppRouter extends Component {
+	render(){
+		return (
+			<HashRouter>
+				<App />
+			</HashRouter>
+		);
+	}
+}
+
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
 registerServiceWorker();
